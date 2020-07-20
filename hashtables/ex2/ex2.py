3136a88c100984be4d3c9936d712358b544fa682
GwenStacey <gwen.c.purcell@gmail.com>
2020-07-19 20:35:27 -0600

    Completed exercise 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,15 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ticket_table = {}
+    route = []
+    for i in tickets:
+        ticket_table[i.source] = i.destination
+    while len(route) < length:
+        if len(route) == 0:
+            route.append(ticket_table.get('NONE'))
+        i = len(route)-1
+        route.append(ticket_table.get(route[i]))
+
 
     return route
