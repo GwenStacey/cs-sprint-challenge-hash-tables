adf3b7ba6d2f17db95e7d113aaa5ac2c57c1d331
GwenStacey <gwen.c.purcell@gmail.com>
2020-07-19 19:48:59 -0600

    Completed exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,24 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weights_indices = {}
+    for i in range(length):
+        if weights[i] not in weights_indices.keys():
+            weights_indices[weights[i]] = [i]
+        else:
+            weights_indices[weights[i]].append(i)
+    for i in weights_indices.keys():
+        if limit-i in weights_indices.keys():
+            if len(weights_indices[i]) > 1:
+                if weights_indices[i][0]>weights_indices[i][1]:
+                    return (weights_indices[i][0], weights_indices[i][1])
+                else:
+                    return (weights_indices[i][1], weights_indices[i][0])
+            if weights_indices[i] > weights_indices[limit-i]:
+                return (weights_indices[i][0], weights_indices[limit-i][0])
+            else:
+                return (weights_indices[limit-i][0], weights_indices[i][0])
+
+    
 
     return None
